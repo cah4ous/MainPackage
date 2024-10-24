08df745673a04cdd94bf7e0e6148dc0c10c0cdec
Александр Троицкий <>
2024-10-25 00:06:07 +0300

    fix

--- Package.swift
+++ Package.swift
@@ -9,8 +9,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "LocalPackage",
-            targets: ["LocalPackage"]),
+            name: "MainPackage",
+            targets: ["MainPackage"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -21,7 +21,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "LocalPackage",
+            name: "MainPackage",
             dependencies: [
                 .product(name: "TestPackage1", package: "TestPackage1"),
                 .product(name: "TestPackage2", package: "TestPackage2")
